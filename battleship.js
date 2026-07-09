ec4cd27bf099430fe8a5480889e045bf1cc1b3cf
Raeesah <raeesahmoola529@gmail.com>
2026-07-09 18:47:55 +0200

    Create Gameboard class with placeShip and receiveAttack functions

--- battleship.js
+++ battleship.js
@@ -15,3 +15,44 @@ this.hit++
     }
   }
 }
+class Gameboard{
+  constructor(){
+    this.board = [
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+    ];
+this.ships = [];
+this.missed = [];
+  }
+  placeShip(ship, row, col, direction){
+if(direction === "horizontal"){
+
+for (let i = 0; i < ship.length; i++) {
+this.board[row][col + i] = ship;
+}
+
+}else if(direction === "vertical"){
+
+for (let i = 0; i < ship.length; i++) {
+  this.board[row + i][col] = ship;
+}
+}else{
+  throw new Error("Invalid");
+}
+
+  }receiveAttack(row, col){
+if(this.board[row][col]){
+  this.board[row][col].hit();
+}else{
+  this.missed.push([row][col]);
+}
+  }
+}
+
+
